c6a4103dd81f5ad4e6a52a5e494b758536beb25c
Blazej Wojciechowski <113915439+BlazejWojciechowski@users.noreply.github.com>
2023-05-07 12:26:56 +0200

    initial commit with basic configuration

---
+++ src/main/java/blaze/docker/DockerApplication.java
@@ -0,0 +1,24 @@
+package blaze.docker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DockerApplication {
+    @Value("${user}")
+    private String user;
+
+    @GetMapping("/")
+    public String get() {
+        return "Hello! " + user;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+
+}
